08f2ccbf6c859cff2d5af6fc92499b5060459b1c
sundaram123krishnan <krishnsundaram@gmail.com>
2023-07-16 23:25:37 +0530

    code refactoring and reading file

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use std::env;
-fn check_args_length(arguments: &Vec<String>) -> Result<usize, String> {
-    if arguments.len() > 1 && arguments.len() < 4 {
-        Ok(arguments.len())
+use std::{env, fs, process};
+
+fn check_args_length(arguments: &Vec<String>) -> Result<i32, String> {
+    if arguments.len() == 3 {
+        Ok(arguments.len() as i32)
     } else {
-        Err(String::from("Enter 2 arguments"))
+        Err(String::from("2 arguments are expected"))
     }
 }
+
+struct Config {
+    search_string: String,
+    filename: String,
+}
+
+impl Config {
+    fn input_parse(args: &Vec<String>) -> Self {
+        Self {
+            search_string: match args.get(1) {
+                Some(s) => s.to_string(),
+                None => panic!("Cannot find the word"),
+            },
+            filename: match args.get(2) {
+                Some(f) => f.to_string(),
+                None => panic!("Cannot find file"),
+            },
+        }
+    }
+}
+
+fn run(input: &Config) {
+    let contents = fs::read_to_string(&input.filename).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(2);
+    });
+    println!("{}", contents);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let sz = check_args_length(&args);
-    let _sz = match sz {
-        Ok(n) => n,
-        Err(_) => panic!("Less arguments provided"),
-    };
 
-    let _search_string = args.get(1);
-    let _filename = args.get(2);
+    check_args_length(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let _search_string = match _search_string {
-        Some(s) => s,
-        None => panic!("Pls enter string to search"),
-    };
-    let _filename = match _filename {
-        Some(f) => f,
-        None => panic!("Pls enter valid filename"),
-    };
+    let input = Config::input_parse(&args);
+    run(&input);
 }
