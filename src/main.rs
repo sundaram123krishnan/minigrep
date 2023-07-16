838d618cb4a28ec949acb79f51e6ea3b411f941f
sundaram123krishnan <krishnsundaram@gmail.com>
2023-07-16 20:25:05 +0530

    added arguments from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+fn check_args_length(arguments: &Vec<String>) -> Result<usize, String> {
+    if arguments.len() > 1 && arguments.len() < 4 {
+        Ok(arguments.len())
+    } else {
+        Err(String::from("Enter 2 arguments"))
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let sz = check_args_length(&args);
+    let _sz = match sz {
+        Ok(n) => n,
+        Err(_) => panic!("Less arguments provided"),
+    };
+
+    let _search_string = args.get(1);
+    let _filename = args.get(2);
+
+    let _search_string = match _search_string {
+        Some(s) => s,
+        None => panic!("Pls enter string to search"),
+    };
+    let _filename = match _filename {
+        Some(f) => f,
+        None => panic!("Pls enter valid filename"),
+    };
 }
